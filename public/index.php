12418c544e519e696a7fce9fc444f1882fe5f557
qingrenyanlei <createsmart@163.com>
2019-10-03 20:45:39 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,12 +4,12 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__ . '/../bootstrap/app.php';
 
-$kennel = new AppKernel('dev', true, __DIR__);
+$kernel = new AppKernel('dev', true, __DIR__);
 
 $request = Request::createFromGlobals();
 
-$response = $kennel->handle($request);
+$response = $kernel->handle($request);
 
 $response->send();
 
-$kennel->terminate($request, $response);
+$kernel->terminate($request, $response);
